f1cbc1385f12571525c26959881413844f1d08a7
Digital House Brasil <99281226+PedagogiaDHBrasil@users.noreply.github.com>
2022-04-20 21:39:12 -0300

    adicionando importação com tsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Banner from "./components/banner";
+import Banner from "./components/banner/index.tsx";
 import Character from "./components/cards/index.tsx";
 import "./styles.css";
 
